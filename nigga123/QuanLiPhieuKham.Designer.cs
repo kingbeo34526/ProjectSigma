a1541886b981c0fe986d2bd21f6ad1946b7dae14
Khoa Tran <anhkhoa260707@gmail.com>
2025-04-01 14:27:38 +0700

    Update layout and sizing of QuanLiPhieuKham form
    Significantly modified the layout of the `QuanLiPhieuKham` form, including adjustments to the positions and sizes of controls such as labels, buttons, and a DataGridView. Increased overall dimensions and added margins for improved spacing. Resized controls for better usability and adjusted font sizes for enhanced readability.

--- nigga123/QuanLiPhieuKham.Designer.cs
+++ nigga123/QuanLiPhieuKham.Designer.cs
@@ -46,28 +46,31 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(21, 266);
+            this.label2.Location = new System.Drawing.Point(32, 409);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.Size = new System.Drawing.Size(163, 23);
             this.label2.TabIndex = 6;
             this.label2.Text = "Lọc theo trạng thái";
             // 
             // DgvPhieuKham
             // 
             this.DgvPhieuKham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvPhieuKham.Location = new System.Drawing.Point(19, 12);
+            this.DgvPhieuKham.Location = new System.Drawing.Point(28, 18);
+            this.DgvPhieuKham.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DgvPhieuKham.Name = "DgvPhieuKham";
             this.DgvPhieuKham.RowHeadersWidth = 62;
-            this.DgvPhieuKham.Size = new System.Drawing.Size(671, 240);
+            this.DgvPhieuKham.Size = new System.Drawing.Size(1006, 369);
             this.DgvPhieuKham.TabIndex = 12;
             this.DgvPhieuKham.SelectionChanged += new System.EventHandler(this.DgvPhieuKham_SelectionChanged);
             // 
             // NutLapHS
             // 
             this.NutLapHS.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NutLapHS.Location = new System.Drawing.Point(697, 112);
+            this.NutLapHS.Location = new System.Drawing.Point(1042, 188);
+            this.NutLapHS.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.NutLapHS.Name = "NutLapHS";
-            this.NutLapHS.Size = new System.Drawing.Size(132, 39);
+            this.NutLapHS.Size = new System.Drawing.Size(198, 46);
             this.NutLapHS.TabIndex = 14;
             this.NutLapHS.Text = "Lập hồ sơ khám bệnh";
             this.NutLapHS.UseVisualStyleBackColor = true;
@@ -77,19 +80,19 @@
             // 
             this.CbTrangThai.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CbTrangThai.FormattingEnabled = true;
-            this.CbTrangThai.Location = new System.Drawing.Point(136, 263);
-            this.CbTrangThai.Margin = new System.Windows.Forms.Padding(2);
+            this.CbTrangThai.Location = new System.Drawing.Point(204, 405);
             this.CbTrangThai.Name = "CbTrangThai";
-            this.CbTrangThai.Size = new System.Drawing.Size(123, 21);
+            this.CbTrangThai.Size = new System.Drawing.Size(182, 31);
             this.CbTrangThai.TabIndex = 18;
             this.CbTrangThai.SelectedIndexChanged += new System.EventHandler(this.CbTrangThai_SelectedIndexChanged);
             // 
             // NutXem
             // 
             this.NutXem.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NutXem.Location = new System.Drawing.Point(697, 69);
+            this.NutXem.Location = new System.Drawing.Point(1042, 112);
+            this.NutXem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.NutXem.Name = "NutXem";
-            this.NutXem.Size = new System.Drawing.Size(132, 39);
+            this.NutXem.Size = new System.Drawing.Size(198, 66);
             this.NutXem.TabIndex = 19;
             this.NutXem.Text = "Xem/Sửa hồ sơ khám bệnh";
             this.NutXem.UseVisualStyleBackColor = true;
@@ -98,9 +101,10 @@
             // NutDonThuoc
             // 
             this.NutDonThuoc.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NutDonThuoc.Location = new System.Drawing.Point(697, 26);
+            this.NutDonThuoc.Location = new System.Drawing.Point(1042, 56);
+            this.NutDonThuoc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.NutDonThuoc.Name = "NutDonThuoc";
-            this.NutDonThuoc.Size = new System.Drawing.Size(132, 39);
+            this.NutDonThuoc.Size = new System.Drawing.Size(198, 46);
             this.NutDonThuoc.TabIndex = 20;
             this.NutDonThuoc.Text = "Đơn thuốc";
             this.NutDonThuoc.UseVisualStyleBackColor = true;
@@ -109,9 +113,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(768, 157);
+            this.button1.Location = new System.Drawing.Point(1148, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 39);
+            this.button1.Size = new System.Drawing.Size(92, 46);
             this.button1.TabIndex = 21;
             this.button1.Text = "Tải lại";
             this.button1.UseVisualStyleBackColor = true;
@@ -120,9 +125,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(697, 202);
+            this.button2.Location = new System.Drawing.Point(1042, 300);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 39);
+            this.button2.Size = new System.Drawing.Size(198, 46);
             this.button2.TabIndex = 22;
             this.button2.Text = "Thoát";
             this.button2.UseVisualStyleBackColor = true;
@@ -131,9 +137,10 @@
             // TxtTimKiemTen
             // 
             this.TxtTimKiemTen.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtTimKiemTen.Location = new System.Drawing.Point(379, 264);
+            this.TxtTimKiemTen.Location = new System.Drawing.Point(568, 406);
+            this.TxtTimKiemTen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TxtTimKiemTen.Name = "TxtTimKiemTen";
-            this.TxtTimKiemTen.Size = new System.Drawing.Size(122, 22);
+            this.TxtTimKiemTen.Size = new System.Drawing.Size(181, 29);
             this.TxtTimKiemTen.TabIndex = 23;
             this.TxtTimKiemTen.TextChanged += new System.EventHandler(this.TxtTimKiemTen_TextChanged);
             // 
@@ -141,18 +148,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(269, 267);
+            this.label1.Location = new System.Drawing.Point(404, 411);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.Size = new System.Drawing.Size(163, 23);
             this.label1.TabIndex = 24;
             this.label1.Text = "Tìm kiếm theo tên:";
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(697, 157);
+            this.button3.Location = new System.Drawing.Point(1042, 244);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(65, 39);
+            this.button3.Size = new System.Drawing.Size(98, 46);
             this.button3.TabIndex = 25;
             this.button3.Text = "Xóa";
             this.button3.UseVisualStyleBackColor = true;
@@ -160,9 +169,9 @@
             // 
             // QuanLiPhieuKham
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 292);
+            this.ClientSize = new System.Drawing.Size(1251, 449);
             this.ControlBox = false;
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
@@ -175,6 +184,7 @@
             this.Controls.Add(this.NutLapHS);
             this.Controls.Add(this.DgvPhieuKham);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "QuanLiPhieuKham";
             this.Text = "Quản lí phiếu khám bệnh";
             this.Load += new System.EventHandler(this.QuanLiPhieuKham_Load);
